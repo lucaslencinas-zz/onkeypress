3a8dd2b004a0bd1898d14fdf45bc9e3bbe4fa560
Lucas Lencinas <lllencinas@gmail.com>
2017-11-18 21:01:00 -0300

    Refactor default valueas

--- src/components/Room/Board/Board.js
+++ src/components/Room/Board/Board.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BOARD_HEIGHT } from '~/utils/constants';
+import {
+  BOARD_HEIGHT,
+  INITIAL_SNAKE_POSITIONS,
+  INITIAL_FOOD_POSITION
+} from '~/utils/constants';
 import Row from './Row';
 import styles from './Board.css';
-
-const INITIAL_SNAKE_POSITIONS = [{ x: 5, y: 1 }, { x: 4, y: 1 }, { x: 3, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 1 }];
-const INITIAL_FOOD_POSITION = { x: 20, y: 20 };
 
 class Board extends React.Component {
   constructor(props) {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,7 @@ module.exports = {
   FOOD_CONTENT: 'food',
   SNAKE_CONTENT: 'snake',
   BOARD_WIDTH: 40,
-  BOARD_HEIGHT: 30
+  BOARD_HEIGHT: 30,
+  INITIAL_SNAKE_POSITIONS: [{ x: 5, y: 1 }, { x: 4, y: 1 }, { x: 3, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 1 }],
+  INITIAL_FOOD_POSITION: { x: 20, y: 20 }
 };
